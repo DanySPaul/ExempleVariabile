b0b0225a2ac0eb234176f25183ad32c559fa80ee
Daniel <danyxup123@gmail.com>
2019-07-27 10:27:05 +0300

    Am adaugat inca  doua mesaje.

--- src/com/sda11/exemple/ExempleVariabile.java
+++ src/com/sda11/exemple/ExempleVariabile.java
@@ -6,6 +6,8 @@ public class ExempleVariabile {
     public static void main(String[] args){
      System.out.println("HelloWorld");
      System.out.println("HelloGIT");
+     System.out.println("Invatam GIT");
+     
 
 
 
